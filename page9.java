486e003978996f5eeb89bd03ef7053089af32f0a
emmachen12 <emc1207pi@gmail.com>
2024-02-07 14:36:52 -0500

    Create page9.java

---
+++ page9.java
@@ -0,0 +1,14 @@
+/************************************
+*Project: Demo for Comments
+*Programmer: Emma Chen
+*Date: Febuary 7, 2024
+*Program Name: page9.java
+*************************************/
+public class page9{
+
+    public static void main (String[] args)
+    {
+    System.out.println("Hi");
+    System.out.println("LabOneA");
+    }
+}
